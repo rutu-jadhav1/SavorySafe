67e4c9e2afb17104476b072f704c64b61be1ce87
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-08-11 11:50:52 +0530

    created models

---
+++ server/models/Recipe.js
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+
+
+ const recipeSchema = new Schema({
+    title : {
+        type : String,
+        require : true,
+    },
+    category : {
+        type : String,
+        default : "other",
+    },
+    image : {
+        type : String,
+        require : true,
+    },
+    ingredients : {
+        type : String,
+        require : true,
+    },
+    description : {
+        type : String,
+        require : true,
+    },
+    user : {
+        type : Schema.Types.ObjectId,
+        ref : "User",
+        require : true
+    }
+ },{
+    timestamps : true
+ })
+
+ const recipe = model('recipe', recipeSchema)
+
+ export default recipe;
