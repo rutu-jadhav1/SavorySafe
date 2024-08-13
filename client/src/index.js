6b48e8ca6bff1c333c78e51a55a74e939e0c7341
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-08-13 11:08:52 +0530

    basic page created

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import {RouterProvider , createBrowserRouter} from 'react-router-dom'
 
 import './index.css';
+import Signup from './views/Signup/Signup';
+import Login from './views/Login/Login';
+import Home from './views/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const router = createBrowserRouter([
   {
     path : '/',
-    element : <h1>Home</h1>
+    element : <Home/>
+  },
+  {
+    path : '/signup',
+    element : <Signup/>
+  },
+  {
+    path : '/login',
+    element : <Login/>
   }
 ])
 
