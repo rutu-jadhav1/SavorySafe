7d5fe92c3f31cb135042d3b7b17d569d8d46a695
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-08-16 12:47:59 +0530

    login page updated

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import loginUser from './user.png'
 import { Link } from 'react-router-dom'
-import toast, {Toaster} from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
 import axios from 'axios'
 
 import './../Signup/Signup.css'
+import loginUser from './user.png'
+import loginSVg from './Login-amico.svg'
 
 
 function Login() {
@@ -14,46 +15,52 @@ function Login() {
 
   const loginNow = async () => {
     const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/login`, {
-      email : email,
-      password : password
+      email: email,
+      password: password
     })
-    if(response.data.success){
+    if (response.data.success) {
       toast.success(response.data.message)
-      localStorage.setItem('currentUser',JSON.stringify(response.data.data))
+      localStorage.setItem('currentUser', JSON.stringify(response.data.data))
       toast.loading('Redirecting to Dashboard...')
 
-      setTimeout(()=>{
+      setTimeout(() => {
         window.location.href = '/'
       }, 3000)
     }
-    else{
+    else {
       toast.error(response.data.message)
     }
   }
   return (
     <div>
+      <div className='login-pg'>
         <form className='register-form text-style'>
-                <div className='register-name-container'>
-                    <h2>Login Here</h2>
-                    <img src={loginUser} alt='register-img' className='register-img' />
-                </div>
-                <div className="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control register-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" 
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}/>
-                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div className="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control register-input" id="exampleInputPassword1" placeholder="Password" 
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}/>
-                </div>
-                <button type="button" className="register-btn" onClick={loginNow}>Login</button>
-                <Link to='/register' className='register-pg-link'>Don't have account? <span className='pg-link-nm'>Register</span></Link>
+          <div className='name-container'>
+            <h2>Login Here</h2>
+            <img src={loginUser} alt='register-img' className='register-img' />
+          </div>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} />
+            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div className="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control login-input" id="exampleInputPassword1" placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <button type="button" className="register-btn" onClick={loginNow}>Login</button>
+          <Link to='/Signup' className='login-pg-link'>Don't have account? <span className='pg-link-nm'>Register</span></Link>
         </form>
-        <Toaster/>
+
+        <div>
+          <img src={loginSVg} className='svg-logo' />
+        </div>
+      </div>
+      <Toaster />
     </div>
   )
 }
